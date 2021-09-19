fe91150a172a6ecc7ff9814ba9446a9c4db84920
Fred Leal <flm_@msn.com>
2021-09-19 20:27:19 -0300

    constumização das tabs

--- src/components/tabs/tablayout.js
+++ src/components/tabs/tablayout.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Tabs, Tab, Panel } from '@bumaga/tabs' 
+import './tablayout.css'
+
+
 
 function TabLayout() {
 
     return (
         <Tabs>
-        <div>
-          <Tab><button>Tab 1</button></Tab>
-          <Tab><button>Tab 2</button></Tab>
-          <Tab><button>Tab 3</button></Tab>
+        <div className="tabs">
+          <Tab><button className="tab">Projetos</button></Tab>
+          <Tab><button className="tab">Certificados</button></Tab>
+          <Tab><button className="tab">Referências</button></Tab>
         </div>
 
         <Panel><p>Panel 1</p></Panel>
         <Panel><p>Panel 2</p></Panel>
-        <Panel><p>panel 3</p></Panel>
+        <Panel><p>Panel 3</p></Panel>
       </Tabs>
     );
   }
