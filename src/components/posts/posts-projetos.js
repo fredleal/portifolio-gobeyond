9c6351c353874000889264b7c7929ce8cbd7ff24
Fred Leal <flm_@msn.com>
2021-09-21 13:41:34 -0300

    adição powerlink direcionando aos repo

--- src/components/posts/posts-projetos.js
+++ src/components/posts/posts-projetos.js
@@ -6,21 +6,21 @@ function PostProjetos() {
         <section className="post-container">
             <div className="post1">
                 <div className="imagem">
-                    <a href="/" className="gb">
+                    <a href="https://github.com/fredleal/moveit" className="gb">
                         <h3 className="text"> Move It</h3>
                      </a>
                 </div>     
             </div>
             <div className="post2">
                 <div className="imagem">
-                    <a href="/" className="gb">
+                    <a href="https://github.com/fredleal/weather-react" className="gb">
                         <h3 className="text"> Weather</h3>
                      </a>
                 </div>     
             </div>
-            <div className="post3">
+            <div className="post3"href="https://github.com/fredleal/rocket-q" >
                 <div className="imagem">
-                    <a href="/" className="gb">
+                    <a href="https://github.com/fredleal/rocket-q" className="gb">
                         <h3 className="text"> Roquet.Q</h3>
                      </a>
                 </div>     
